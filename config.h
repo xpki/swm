05b72175c0f82d24fd8b4ce7b393c70e19cbc800
xpki <125104376+xpki@users.noreply.github.com>
2025-06-18 20:55:18 -0500

    Update config.h

--- config.h
+++ config.h
@@ -1,5 +1,9 @@
 /* config.h */
-
+static const int ws1 = 1;
+static const int ws2 = 2;
+static const int ws3 = 3;
+static const int ws4 = 4;
+static const int ws5 = 5;
 static const unsigned int borderpx = 2;
 static const char *termcmd[] = { "st", NULL };
 static const char *dmenucmd[] = { "dmenu_run", NULL };
@@ -13,4 +17,18 @@ static const Key keys[] = {
     { MODKEY, XK_c, killclient, NULL },
     { MODKEY, XK_j, focusnext, NULL },
     { MODKEY, XK_k, focusprev, NULL },
+
+    // Switch workspace
+    { MODKEY,             XK_1, view,     &(Arg){.v = &ws1} },
+    { MODKEY,             XK_2, view,     &(Arg){.v = &ws2} },
+    { MODKEY,             XK_3, view,     &(Arg){.v = &ws3} },
+    { MODKEY,             XK_4, view,     &(Arg){.v = &ws4} },
+    { MODKEY,             XK_5, view,     &(Arg){.v = &ws5} },
+
+    // Move focused window to workspace
+    { MODKEY | ShiftMask, XK_1, movetows, &(Arg){.v = &ws1} },
+    { MODKEY | ShiftMask, XK_2, movetows, &(Arg){.v = &ws2} },
+    { MODKEY | ShiftMask, XK_3, movetows, &(Arg){.v = &ws3} },
+    { MODKEY | ShiftMask, XK_4, movetows, &(Arg){.v = &ws4} },
+    { MODKEY | ShiftMask, XK_5, movetows, &(Arg){.v = &ws5} },
 };
