1596736d3a4c3668dff2957210ca561286bf63c7
xpki <125104376+xpki@users.noreply.github.com>
2025-06-18 20:45:03 -0500

    Update config.h

--- config.h
+++ config.h
@@ -1,9 +1,16 @@
+/* config.h */
+
 static const unsigned int borderpx = 2;
-static const char *termcmd[] = { "xterm", NULL };
+static const char *termcmd[] = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 #define MODKEY Mod1Mask  // Alt
 
 static const Key keys[] = {
     { MODKEY, XK_Return, spawn, &(Arg){.v = termcmd} },
+    { MODKEY, XK_p, spawn, &(Arg){.v = dmenucmd} },
     { MODKEY, XK_q, quit, NULL },
-    { MODKEY, XK_i, toggle_layout, NULL },  
+    { MODKEY, XK_t, toggle_layout, NULL },
+    { MODKEY, XK_c, killclient, NULL },
+    { MODKEY, XK_j, focusnext, NULL },
+    { MODKEY, XK_k, focusprev, NULL },
 };
